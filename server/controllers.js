6c0dcc180e7158af4a7159b42469e93451d82645
Thomas Harbert <tgharbert3@gmail.com>
2023-12-07 13:18:47 -0600

    fixed another merge conflict

--- server/controllers.js
+++ server/controllers.js
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
 const db = require('./db');
 
 module.exports = {
   // handle requests and use sequelize to hanldle req, res
-=======
-const db = require('./db.js');
-
-module.exports = {
->>>>>>> 3d911d24d18022bb95d15a201346845d9b9357bf
   questions: {
     getQuestions: (req, res) => {},
     postQuestion: (req, res) => {},
--- server/router.js
+++ server/router.js
@@ -2,7 +2,6 @@ const router = require('express').Router();
 const controllers = require('./controllers');
 
 // write the routes based on the controllers...
-<<<<<<< HEAD
 router.get('/qa/questions', controllers.questions.getQuestions);
 router.post('/qa/questions', controllers.questions.postQuestion);
 router.put(
@@ -32,26 +31,3 @@ router.put(
 );
 
 module.exports = router;
-=======
-router.route('/qa/questions').get(controllers.questions.getQuestions);
-router.route('/qa/questions').post(controllers.questions.postAnswer);
-router
-  .route('/qa/questions/:question_id/report')
-  .put(controllers.questions.helpfulQuestion);
-router
-  .route('/qa/questions/:question_id/helpful')
-  .put(controllers.questions.reportQuestion);
-
-router
-  .route('/qa/questions/:question_id/answers')
-  .get(controllers.answers.getAnswers);
-router
-  .route('/qa/questions/:question_id/answers')
-  .post(controllers.answers.postAnswer);
-router
-  .route('/qa/questions/:answer_id/report')
-  .put(controllers.answers.helpfulAnswer);
-router
-  .route('/qa/questions/:answer_id/helpful')
-  .put(controllers.answers.reportAnswer);
->>>>>>> 3d911d24d18022bb95d15a201346845d9b9357bf
